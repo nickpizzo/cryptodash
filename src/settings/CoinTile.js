dc764d0774c3e8c2788d1800e5038dae881c0ce5
Nick Pizzo <nick.pizzo@gmail.com>
2019-05-12 09:55:53 -0700

    add/remove favorites layout and styles

--- src/settings/CoinTile.js
+++ src/settings/CoinTile.js
@@ -1,18 +1,25 @@
 import React, { useContext } from "react";
 import { AppContext } from "../app/AppProvider";
-import { SelectableTile } from "../shared/Tile";
+import { SelectableTile, DisabledTile, DeletableTile } from "../shared/Tile";
 import CoinHeaderGrid from "./CoinHeaderGrid";
 import CoinImage from "../shared/CoinImage";
 
-export default function({ coinKey }) {
+export default function({ coinKey, topSection }) {
   const { coinList } = useContext(AppContext);
-  const TileClass = SelectableTile;
+  let TileClass = SelectableTile;
+  if (topSection) {
+    TileClass = DeletableTile;
+  }
 
-  let coin = coinList[coinKey];
+  const coin = coinList[coinKey];
 
   return (
     <TileClass>
-      <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol} />
+      <CoinHeaderGrid
+        topSection={topSection}
+        name={coin.CoinName}
+        symbol={coin.Symbol}
+      />
       <CoinImage coin={coin} />
     </TileClass>
   );
