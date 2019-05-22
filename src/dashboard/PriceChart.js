82a8685a2b5b78ca5ac6382fb99105bf0866cadc
Nick Pizzo <nick.pizzo@gmail.com>
2019-05-22 12:04:16 -0700

    highcharts theme

--- src/dashboard/PriceChart.js
+++ src/dashboard/PriceChart.js
@@ -3,6 +3,8 @@ import HighchartsConfig from "./HighchartsConfig";
 import { Tile } from "../shared/Tile";
 import { AppContext } from "../app/AppProvider";
 import ReactHighcharts from "react-highcharts";
+import HighchartsTheme from "./HighchartsTheme";
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
 
 export default function PriceChart() {
   const { currentFavorite, setCurrentFavorite } = useContext(AppContext);
